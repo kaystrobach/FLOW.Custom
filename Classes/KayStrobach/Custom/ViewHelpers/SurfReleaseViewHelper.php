64f6a9da0c7dd147143c4e124907cf400c2f763b
Kay Strobach <github@kay-strobach.de>
2014-05-04 13:23:38 +0200

    [FEATURE] Add SurfReleaseViewHelper

---
+++ Classes/KayStrobach/Custom/ViewHelpers/SurfReleaseViewHelper.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kay
+ * Date: 04.05.14
+ * Time: 12:52
+ */
+
+namespace KayStrobach\Custom\ViewHelpers;
+
+use TYPO3\FLOW\Annotations as Flow;
+
+class SurfReleaseViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\Core\Bootstrap
+	 */
+	protected $bootstrap;
+
+	/**
+	 * @param string $format
+	 * @return string
+	 */
+	public function render($format = 'd.m.Y H:i:s') {
+		if($this->bootstrap->getContext()->isProduction()) {
+			if(@is_link(FLOW_PATH_FLOW)) {
+				$link = readlink(FLOW_PATH_FLOW);
+				$date = \DateTime::createFromFormat(
+					'Ymdhis',
+					substr($link, 3)
+				);
+				$output = 'Production Context, with last build from: ' . $date->format($format);
+			} else {
+				return 'not deployed with TYPO3.Surf?';
+			}
+		} elseif($this->bootstrap->getContext()->isDevelopment()) {
+			$output = 'Development Contenxt';
+		} else {
+			$output = $this->renderChildren();
+		}
+		return $output;
+	}
+} 
