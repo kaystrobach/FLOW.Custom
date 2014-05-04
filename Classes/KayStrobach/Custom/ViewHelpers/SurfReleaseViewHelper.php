8490260c9fbcd9ccf4bd745f17b4f204b6cdb174
Kay Strobach <github@kay-strobach.de>
2014-05-04 15:41:01 +0200

    remove the debugging || 1

--- Classes/KayStrobach/Custom/ViewHelpers/SurfReleaseViewHelper.php
+++ Classes/KayStrobach/Custom/ViewHelpers/SurfReleaseViewHelper.php
@@ -24,7 +24,7 @@ class SurfReleaseViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHel
 	 * @return string
 	 */
 	public function render($format = 'd.m.Y H:i:s') {
-		if($this->bootstrap->getContext()->isProduction() || 1) {
+		if($this->bootstrap->getContext()->isProduction()) {
 			$link = basename(FLOW_PATH_ROOT);
 			if(((integer) $link !== 0) && (strlen($link) === 14)) {
 				$date = \DateTime::createFromFormat(
