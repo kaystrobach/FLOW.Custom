87a208a72710c7007d7f96cbad1cf979f180b15e
Kay Strobach <info@kay-strobach.de>
2015-11-16 10:07:49 +0100

    [TASK] add logic to hide certain pickers

--- Resources/Public/datetimepicker.js
+++ Resources/Public/datetimepicker.js
@@ -4,8 +4,22 @@ $( document ).ready(
         jQuery.each(
             jQuery('[data-date-format]'),
             function(index, value) {
+                var format = $(value).attr('data-date-format');
+                var enableDatePicker = true;
+                var enableTimePicker = true;
+
+                if((format.indexOf('H') < 0) && (format.indexOf('H') < 0)) {
+                    enableTimePicker = false;
+                }
+
+                if((format.indexOf('d') < 0) && (format.indexOf('m') < 0) && (format.indexOf('Y') < 0) && (format.indexOf('Y') < 0)) {
+                    enableDatePicker = false;
+                }
+
                 jQuery(value).datetimepicker({
-                    format: $(value).attr('data-date-format'),
+                    datepicker: enableDatePicker,
+                    timepicker: enableTimePicker,
+                    format: format,
                     mask:true,
                     todayButton: true,
                     defaultSelect: true,
