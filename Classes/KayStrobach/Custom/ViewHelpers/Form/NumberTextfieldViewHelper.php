71c8a2cc548c15ca9a7f049524429ee3f65259e3
Kay Strobach <info@kay-strobach.de>
2017-01-20 08:35:07 +0100

    [TASK] respec property mapping with the numberfield

--- Classes/KayStrobach/Custom/ViewHelpers/Form/NumberTextfieldViewHelper.php
+++ Classes/KayStrobach/Custom/ViewHelpers/Form/NumberTextfieldViewHelper.php
@@ -22,26 +22,60 @@ class NumberTextfieldViewHelper extends TextfieldViewHelper
     {
         parent::initializeArguments();
         $this->registerArgument('decimals', 'int', '', false, 2);
-        $this->registerArgument('decimalSeparator', 'int', '', false, '.');
-        $this->registerArgument('thousandsSeparator', 'int', '', false, ',');
+        $this->registerArgument('decimalSeparator', 'string', '', false, '.');
+        $this->registerArgument('thousandsSeparator', 'string', '', false, ',');
+    }
+
+    public function render($required = false, $type = 'text')
+    {
+        if ($this->arguments['thousandsSeparator'] === $this->arguments['decimalSeparator']) {
+            $this->arguments['thousandsSeparator'] = '';
+        }
+
+        $name = $this->getName();
+        $this->registerFieldNameForFormTokenGeneration($name);
+
+        $this->tag->addAttribute('name', $name . '[value]');
+        $this->tag->addAttribute('type', $type);
+
+        $value = $this->getValueAttribute();
+        if ($value !== null) {
+            $this->tag->addAttribute('value', $value);
+        }
+
+        if ($required === true) {
+            $this->tag->addAttribute('required', 'required');
+        }
+        $this->addAdditionalIdentityPropertiesIfNeeded();
+        $this->setErrorClassAttribute();
+
+        $buffer = $this->tag->render();
+        $buffer .= '<input type="hidden" value="' . htmlspecialchars($this->arguments['decimalSeparator']) . '" name="' . $this->getName() . '[decimalSeparator]" />';
+        $buffer .= '<input type="hidden" value="' . htmlspecialchars($this->arguments['thousandsSeparator']) . '" name="' . $this->getName() . '[thousandsSeparator]" />';
+        return $buffer;
     }
 
     /**
-     * Get the value of this form element.
-     * Either returns arguments['value'], or the correct value for Object Access.
+     * Returns the current value of this Form ViewHelper and converts it to an identifier string in case it's an object
+     * The value is determined as follows:
+     * * If property mapping errors occurred and the form is re-displayed, the *last submitted* value is returned
+     * * Else the bound property is returned (only in objectAccessor-mode)
+     * * As fallback the "value" argument of this ViewHelper is used
      *
-     * @param boolean $convertObjects whether or not to convert objects to identifiers
+     * @param boolean $ignoreSubmittedFormData By default the submitted form value has precedence over value/property argument upon re-display. With this flag set the submitted data is not evaluated (e.g. for checkbox and hidden fields where the value attribute should not be changed)
      * @return mixed Value
      */
-    protected function getValue($convertObjects = true)
+    protected function getValueAttribute($ignoreSubmittedFormData = false)
     {
-        $value = parent::getValue($convertObjects);
-        $output = number_format(
-            (float)$value,
-            $this->arguments['decimals'],
-            $this->arguments['decimalSeparator'],
-            $this->arguments['thousandsSeparator']
-        );
-        return $output;
+        $value = parent::getValueAttribute($ignoreSubmittedFormData);
+        if (is_numeric($value)) {
+            $value = number_format(
+                (float)$value,
+                $this->arguments['decimals'],
+                $this->arguments['decimalSeparator'],
+                $this->arguments['thousandsSeparator']
+            );
+        }
+        return $value;
     }
 }
