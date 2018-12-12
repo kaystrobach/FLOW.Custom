eb97a681ad3d99f00cac52487e56408e0686f4ee
Kay Strobach <github@kay-strobach.de>
2018-12-12 12:03:16 +0100

    Update SurfReleaseViewHelper.php

--- Classes/ViewHelpers/SurfReleaseViewHelper.php
+++ Classes/ViewHelpers/SurfReleaseViewHelper.php
@@ -1,53 +1,75 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kay
- * Date: 04.05.14
- * Time: 12:52
- */
 
 namespace KayStrobach\Custom\ViewHelpers;
 
-use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Core\ApplicationContext;
+use Neos\Flow\Core\Bootstrap;
+use Neos\Flow\Utility\Environment;
 
 class SurfReleaseViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractViewHelper {
-	/**
-	 * @Flow\Inject
-	 * @var \Neos\Flow\Core\Bootstrap
-	 */
-	protected $bootstrap;
-
-	/**
-	 * Show release string based on symlink
-	 *
-	 * @param string $format
-	 * @return string
-	 */
+    /**
+     * Show release string based on symlink
+     *
+     * @param string $format
+     * @return string
+     * @throws \Exception
+     */
 	public function render($format = 'd.m.Y H:i:s') {
-		if($this->bootstrap->getContext()->isProduction()) {
-			$link = basename(FLOW_PATH_ROOT);
-			if(((integer) $link !== 0) && (strlen($link) === 14)) {
-				$date = \DateTime::createFromFormat(
-					'YmdHis',
-					$link
-				);
-				if($date === FALSE) {
-					$output = 'Build: ' . $link;
-				} else {
-					$output = 'Build: ' . $date->format($format);
-				}
-				$revisionFilename = FLOW_PATH_ROOT . '../' . $link . 'REVISION';
-				if(is_file($revisionFilename)) {
-					$output .= ' / @Rev.: ' . file_get_contents($revisionFilename);
-				}
-			} else {
-				return 'No TYPO3.Surf? Build: ' . $link;
-			}
-		} elseif($this->bootstrap->getContext()->isDevelopment()) {
-			$output = 'Development Context';
-		} else {
-			$output = $this->renderChildren();
-		}
+		$output = $this->getContext() . ': ';
+	    $date = $this->extractDateFromRevisionFile();
+	    if ($date === null) {
+	        $date = $this->extractDateFromSurfPath();
+        }
+	    if ($date === null) {
+	        $date = new \DateTime('now');
+        }
+
+	    $output.= $date->format($format);
+	    $output.= $this->extractRevisionFileContent();
+
 		return $output;
 	}
-} 
+
+    /**
+     * @return \Neos\Flow\Core\ApplicationContext
+     */
+	protected function getContext(): ApplicationContext
+    {
+        $objectManager = Bootstrap::$staticObjectManager;
+        $environment = $objectManager->get(Environment::class);
+        return $environment->getContext();
+
+    }
+
+	protected function extractDateFromRevisionFile()
+    {
+        if (!file_exists(FLOW_PATH_ROOT . '/REVISION')) {
+            return null;
+        }
+        $dt = new \DateTime('now');
+        $dt->setTimestamp(filemtime(FLOW_PATH_ROOT . '/REVISION'));
+        return $dt;
+    }
+
+	protected function extractDateFromSurfPath()
+    {
+        $link = basename(FLOW_PATH_ROOT);
+        if(((integer) $link !== 0) && (strlen($link) === 14)) {
+            $date = \DateTime::createFromFormat(
+                'YmdHis',
+                $link
+            );
+            return $date;
+
+        }
+        return null;
+    }
+
+    protected function extractRevisionFileContent()
+    {
+        if(is_file(FLOW_PATH_ROOT . '/REVISION')) {
+            return ' / @Rev.: ' . file_get_contents(FLOW_PATH_ROOT . '/REVISION');
+        }
+        return '';
+    }
+}
