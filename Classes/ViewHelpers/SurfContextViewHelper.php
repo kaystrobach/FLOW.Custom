5b389f16c7c05dd05434df435739dbea21e3b7d0
Kay Strobach <github@kay-strobach.de>
2018-12-12 11:23:42 +0100

    Update SurfContextViewHelper.php

--- Classes/ViewHelpers/SurfContextViewHelper.php
+++ Classes/ViewHelpers/SurfContextViewHelper.php
@@ -1,35 +1,42 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kay
- * Date: 04.05.14
- * Time: 13:25
- */
 
 namespace KayStrobach\Custom\ViewHelpers;
 
-use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Core\Bootstrap;
+use Neos\Flow\Utility\Environment;
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 
 class SurfContextViewHelper extends \Neos\FluidAdaptor\Core\ViewHelper\AbstractConditionViewHelper{
-	/**
-	 * @Flow\Inject
-	 * @var \Neos\Flow\Core\Bootstrap
-	 */
-	protected $bootstrap;
+	public function initializeArguments()
+    {
+        parent::initializeArguments();
+        $this->registerArgument('context', 'string', 'Context.', false);
+    }
 
-	/**
-	 * Condition for Context
-	 *
-	 * @param string $context
-	 * @return string
-	 */
-	public function render($context = 'Development') {
-		$contextObject = $this->bootstrap->getContext();
-
-		if( (((string)$contextObject) === $context)) {
-			return $this->renderThenChild();
-		} else {
-			return $this->renderElseChild();
-		}
-	}
+    /**
+     * Static method which can be overridden by subclasses. If a subclass
+     * requires a different (or faster) decision then this method is the one
+     * to override and implement.
+     *
+     * Note: method signature does not type-hint that an array is desired,
+     * and as such, *appears* to accept any input type. There is no type hint
+     * here for legacy reasons - the signature is kept compatible with third
+     * party packages which depending on PHP version would error out if this
+     * signature was not compatible with that of existing and in-production
+     * subclasses that will be using this base class in the future. Let this
+     * be a warning if someone considers changing this method signature!
+     *
+     * @param array|NULL $arguments
+     * @param RenderingContextInterface $renderingContext
+     * @return boolean
+     * @api
+     */
+    protected static function evaluateCondition($arguments = null, RenderingContextInterface $renderingContext)
+    {
+        $objectManager = Bootstrap::$staticObjectManager;
+        $environment = $objectManager->get(Environment::class);
+        $contextObject = $environment->getContext();
+        $contextString = (string)$contextObject;
+        return ($contextString === $arguments['context']);
+    }
 }
