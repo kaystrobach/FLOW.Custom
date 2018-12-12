f8c9caf8a77644d9552dc2e4ec19da5205430b3f
Kay Strobach <github@kay-strobach.de>
2018-12-12 12:56:33 +0100

    Update MediaTypeUtility.php

--- Classes/Utility/MediaTypeUtility.php
+++ Classes/Utility/MediaTypeUtility.php
@@ -6,16 +6,6 @@ use Neos\Utility\MediaTypes;
 class MediaTypeUtility {
 
 	public static function getMediaTypeFromResource(\Neos\Flow\ResourceManagement\PersistentResource $resource) {
-		if(function_exists('finfo_open')) {
-			$finfo = new \finfo(FILEINFO_MIME_TYPE);
-			$mimetype = $finfo->file($resource->getUri());
-			if ($mimetype !== FALSE) {
-				return $mimetype;
-			}
-		}
-		if(function_exists('mime_content_type')) {
-			return mime_content_type($resource->getUri());
-		}
-		return MediaTypes::getMediaTypeFromFilename('x.' . $resource->getFileExtension());
+        return $resource->getMediaType();
 	}
 }
